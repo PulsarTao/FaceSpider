cad28ab17b9008a7bb2fbbfad7e214fed55fd48c
小V <2571440661@qq.com>
2016-08-16 01:46:10 +0800

    cv246

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.venus.form.*;
 
 public class Main  {
 	public static void main(String[] args) throws Exception {
-		//加载opencv运行库
+		//加载opencv246运行库
 		System.loadLibrary("opencv_java246");
 		//启动主窗口线程
 		formThead formThead=new formThead("MainForm");
